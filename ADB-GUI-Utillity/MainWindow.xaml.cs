d61c60e0305df5318beeaec10927bb1f17d4bb4e
herb786 <herbertacg@gmail.com>
2018-05-08 17:28:23 -0500

    Set environment variables

--- ADB-GUI-Utillity/MainWindow.xaml.cs
+++ ADB-GUI-Utillity/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
 
 using System.Runtime.InteropServices;
 
@@ -25,6 +27,10 @@ namespace ADB_GUI_Utillity
     {
         string[] coms = { "-es", "-ez", "-ei", "-el", "-ef" };
         string[] names = { "ExtraString", "ExtraBool", "ExtraInt", "ExtraLong", "ExtraFloat" };
+        public const int OPT_ACTIVITY = 1;
+        public const int OPT_SERVICE = 2;
+        int adbIntent;
+        IList<string> envs = new List<string>();
 
         public MainWindow()
         {
@@ -51,7 +57,83 @@ namespace ADB_GUI_Utillity
             combo1.ItemsSource = names;
             combo2.ItemsSource = names;
             combo3.ItemsSource = names;
+            radButService.IsChecked = true;
 
+            string userprofile = Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+            txtHome.Text = userprofile + "\\AppData\\Local\\Android\\Sdk";
+            txtNDK.Text = userprofile + "\\AppData\\Local\\Android\\Sdk\\ndk-bundle";
+            txtTools.Text = userprofile + "\\AppData\\Local\\Android\\Sdk\\tools";
+            txtPlatform.Text = userprofile + "\\AppData\\Local\\Android\\Sdk\\platform-tools";
+            txtBuild.Text = userprofile + "\\AppData\\Local\\Android\\Sdk\\build-tools\\27.0.3";
+
+        }
+
+        private void Set_Paths(object sender, RoutedEventArgs e)
+        {
+            if (checkPlatform.IsChecked.Value) {
+                envs.Add(txtPlatform.Text);
+            }
+            if (checkTools.IsChecked.Value)
+            {
+                envs.Add(txtTools.Text);          
+            }
+            if (checkBuild.IsChecked.Value)
+            {
+                envs.Add(txtBuild.Text);
+            }
+            add_env_path(String.Join(";", envs.ToArray()));
+            if (checkHome.IsChecked.Value)
+            {
+                set_env_path("ANDROID_HOME", txtHome.Text);
+            }
+            if (checkNDK.IsChecked.Value)
+            {
+                set_env_path("ANDROID_NDK_HOME", txtNDK.Text);
+            }
+            //Application.Current.Shutdown();
+            //System.Windows.Forms.Application.Restart();
+          
+        }
+
+        private void set_env_path(string key, string value)
+        {
+            Process sett = new Process();
+            sett.StartInfo.Verb = "runas";
+            sett.StartInfo.UseShellExecute = false;
+            sett.StartInfo.RedirectStandardOutput = true;
+            sett.StartInfo.FileName = "setx";
+            sett.StartInfo.Arguments = " \"" + key + "\" " + value + " /m";
+            sett.Start();
+            sett.WaitForExit();
+        }
+
+        private void add_env_path(string value)
+        {
+            string path = Environment.ExpandEnvironmentVariables("%PATH%");
+            //string paths = " PATH2 \"%PATH%;" + value + "\" /m";
+            string paths = path + ";" + value;
+            Console.WriteLine(paths);
+            /*
+            Process sett = new Process();
+            sett.StartInfo.Verb = "runas";
+            sett.StartInfo.UseShellExecute = false;
+            sett.StartInfo.RedirectStandardOutput = true;
+            sett.StartInfo.FileName = "setx";
+            sett.StartInfo.Arguments = path;
+            sett.Start();
+            sett.WaitForExit();
+            */
+            Environment.SetEnvironmentVariable("Path", paths, EnvironmentVariableTarget.Machine);
+        }
+
+        private void Activity_Checked(object sender, RoutedEventArgs e)
+        {
+            adbIntent = OPT_ACTIVITY;
+        }
+
+        private void Service_Checked(object sender, RoutedEventArgs e)
+        {
+            adbIntent = OPT_SERVICE;
         }
 
         private void Execute_Click(object sender, RoutedEventArgs e)
@@ -62,12 +144,24 @@ namespace ADB_GUI_Utillity
             adb.StartInfo.RedirectStandardOutput = true;
             adb.StartInfo.FileName = "adb.exe";
             string args = " shell am startservice -n ";
+            if (adbIntent == OPT_ACTIVITY)
+            {
+                args = " shell am start -n ";
+            }
             args = args + txtBoxPackage.Text + "/" + txtBoxTarget.Text;
             args = args + " -a " + txtBoxAction.Text;
-            if (combo1.SelectedIndex > -1)
+            if (combo1.SelectedIndex > -1  && adbIntent == OPT_SERVICE)
             {
                 args = args + " " + coms[combo1.SelectedIndex] + " " + txtBoxName1.Text + " " + txtBoxVal1.Text;
-            }         
+            }
+            if (combo2.SelectedIndex > -1 && adbIntent == OPT_SERVICE)
+            {
+                args = args + " " + coms[combo2.SelectedIndex] + " " + txtBoxName2.Text + " " + txtBoxVal2.Text;
+            }
+            if (combo3.SelectedIndex > -1 && adbIntent == OPT_SERVICE)
+            {
+                args = args + " " + coms[combo3.SelectedIndex] + " " + txtBoxName3.Text + " " + txtBoxVal3.Text;
+            }
             adb.StartInfo.Arguments = args;
             adb.Start();
             MessageBox.Show("Done\nTip: Set export to True\n");
@@ -81,7 +175,22 @@ namespace ADB_GUI_Utillity
             Properties.Settings.Default.Save();
         }
 
+        private void Open_Path_Setup(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("control", "sysdm.cpl");
+        }
+
+        private void Open_Folder(object sender, RoutedEventArgs e)
+        {
+            string userprofile = Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+            string path = userprofile + "\\AppData\\Local\\Android\\Sdk\\platforms\\android-22\\data\\res";
+            Console.WriteLine(path);
+            Process.Start("explorer.exe", path);
+        }
+
     }
+
+    
 
     public class Bundle
     {
